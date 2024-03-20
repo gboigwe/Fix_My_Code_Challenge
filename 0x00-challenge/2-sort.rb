095dc4f429d67759f70b05f86a98316993c57d96
gboigwe <geakande@gmail.com>
2024-03-20 12:09:11 +0100

    Fixed this ruby code

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -10,7 +10,7 @@ ARGV.each do |arg|
     next if arg !~ /^-?[0-9]+$/
 
     # convert to integer
-    i_arg = arg:to_i
+    i_arg = arg.to_i
     
     # insert result at the right position
     is_inserted = false
@@ -18,10 +18,10 @@ ARGV.each do |arg|
     l = result.size
     while !is_inserted && i < l do
         if result[i] < i_arg
-            i += 1
-        else
             result.insert(i - 1, i_arg)
             is_inserted = true
+        else
+            i += 1
             break
         end
     end
