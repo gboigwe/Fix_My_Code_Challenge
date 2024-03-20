5333a9f86f4cb9175eb6506f76d31b3f85a2cddc
gboigwe <geakande@gmail.com>
2024-03-20 13:19:08 +0100

    Fixed this python password validating code

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -53,9 +53,9 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
-        elif self.__password is None:
+        if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().lower()
+        return hashlib.md5(pwd.encode()).hexdigest().lower() = self.__password
 
 
 if __name__ == '__main__':
